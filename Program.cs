83ffa760b841d533da78186ab4cc985fcadd64f1
TassioTristao <tassio.tristao05@gmail.com>
2025-09-18 00:21:29 -0300

    Add initial reservation example with guests and suite

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Hotel.Models;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+// --- Criando hóspedes ---
+var guests = new List<Customer>
+{
+    new Customer("Adam", "Sandler"),
+    new Customer("Cleiton", "Rasta"),
+    new Customer("Tiago", "Miguel")
+};
+
+// --- Criando suíte ---
+var suite = new Suite(suiteType: "Premium", capacity: 2, pricePerNight: 30m);
+
+// --- Criando reserva ---
+var reservation = new Reservation(
+    guests: guests,
+    suite: suite,
+    reservationDays: 5
+);
+
+// --- Exibindo informações ---
+Console.WriteLine($"Hóspedes: {reservation.GetGuestsQuantity}");
+Console.WriteLine($"Valor total da reserva: {reservation.CalculateTotalPrice():C}");
